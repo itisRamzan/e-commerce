eb3d22ad12de0b81534daaf4f6dfc23ee8fff248
Mohd Ramzan Shareef <107868540+itisRamzan@users.noreply.github.com>
2024-03-01 09:27:35 +0530

    Add Suspense component to UserForgotPassword page

--- app/(routes)/(user)/(auth)/forgotpassword/page.js
+++ app/(routes)/(user)/(auth)/forgotpassword/page.js
@@ -1,9 +1,12 @@
 import ForgotPassword from "@/app/ui/user/auth/ForgotPassword";
+import { Suspense } from "react";
 
 export default function UserForgotPassword() {
     return (
         <>
+        <Suspense fallback={<div>Loading...</div>}>
             <ForgotPassword />
+        </Suspense>
         </>
     )
 }
